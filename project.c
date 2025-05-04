a8523914ed7c040a5d950ff17b3c9a6dc74acdac
ahasan mugdho <ahasan.mugdho.cse10.bu@gmail.com>
2025-05-04 15:43:56 +0600

    Update project.c

--- project.c
+++ project.c
@@ -1,12 +1,30 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+void solution(){
+    int n,x;cin>>n>>x;
+    int bits=__builtin_popcountll(x);
+    if (n<=bits){cout<<x;return;}
+    if ((n-bits)%2==0)cout<<x+n-bits;
+    else{
+        if (x>1){cout<<x+n-bits+1;return;}
+        if (x==1){cout<<n+3;return;}
+        else{
+            if (n==1){cout<<-1;return;}
+            else cout<<n+3;
+        }
+    }
+}
 
 int main() {
-    int t, n;
-    scanf("%d", &t);
-    while (t--) {
-        scanf("%d", &n);
-        int max_f = (n * n) / 4; 
-        printf("%d\n", max_f + 1);
-    }
-    return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t=1; 
+    cin>>t;
+    while (t--){
+        solution();
+        cout << '\n';
+    } return 0;
 }
